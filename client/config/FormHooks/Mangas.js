f1880a1632a899c501b9b365a8cf766781890bb5
WeeabooDono <corentin.beuchet@hotmail.fr>
2018-03-14 14:01:57 +0100

    commit gitignore

--- client/config/FormHooks/Mangas.js
+++ client/config/FormHooks/Mangas.js
@@ -27,33 +27,5 @@ AutoForm.hooks({
         onError: function (formType, err) {
             alert(err.reason)
         }
-    },
-    'addTomes': {
-        onSubmit: function (doc) {
-            console.log(doc)
-            Mangas.update(
-                {_id: doc._id},
-                {
-                    $set:
-                        {
-                            tomes: doc.tomes ? doc.tomes: {}
-                        },
-
-            }, function(err){
-                if(err){
-                    error = new Error("Une erreur s'est produite");
-                }
-            });
-            //handeled by type="update"
-            return false; // Dans tout les cas, arrete la soumission des donneés.
-        },
-
-        onSuccess: function () {
-            Router.go(Utils.pathFor('admin.mangas'));
-        },
-
-        onError: function (formType, err) {
-            alert(err.reason)
-        }
     }
 });
